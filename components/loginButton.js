8232f05ca360635ebc292f33cc30e507917eaa0f
MaggieChafee <maggie.chafee@gmail.com>
2023-09-08 17:40:46 -0500

    fixing code

--- components/loginButton.js
+++ components/loginButton.js
@@ -2,13 +2,7 @@ import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  let domString = '';
-  domString += `
-  <div id="welcome">
-    <h3>Welcome to Vocab-YOU-lary!</h3>
-    <button id="google-auth" class="btn btn-dark">GOOGLE LOGIN</button>
-  </div>
-  `;
+  const domString = '<button id="google-auth" class="btn btn-dark">GOOGLE LOGIN</button>';
   document.querySelector('#main-container').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
